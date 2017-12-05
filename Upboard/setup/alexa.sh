60ff1f6b6aa0a195a1b1fa2feabf9858752a8a5a
Alfredo Reyes <reyes-fred@hotmail.com>
2017-12-05 13:47:59 -0800

    Update

---
+++ Upboard/setup/alexa.sh
@@ -0,0 +1,15 @@
+echo
+echo "Welcome to Alexa Linux"
+echo
+
+sleep 4
+
+cd ~/Amikoo
+mkdir Alexa
+cd ~/Amikoo/Alexa
+
+git clone https://github.com/devicehive/AlexaDevice.git
+
+sudo apt install python3-pip git ffmpeg swig libportaudio2 portaudio19-dev libpulse-dev
+sudo pip3 install requests 'git+https://github.com/moaxey/python-zeroconf' pocketsphinx pyaudio
+
